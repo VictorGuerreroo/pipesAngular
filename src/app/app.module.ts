c330f3ba98bd4862fce080b0353417d35f33ad6a
Victor <v.g.m_@hotmail.com>
2021-02-15 20:41:20 +0100

    Pipes de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+
+import { AppRouterModule } from './app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el local de la app
+import  localeEs  from "@angular/common/locales/es";
+import  localeFR  from "@angular/common/locales/fr";
+import { registerLocaleData } from "@angular/common";
+registerLocaleData( localeEs);
+registerLocaleData( localeFR);
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue:'es'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
